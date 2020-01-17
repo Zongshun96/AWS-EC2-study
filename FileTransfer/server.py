f74ca45c6c2dbe4aadf7ee28e1ba6feeff9e28a7
zhan4475 <zhan4475@umn.edu>
2020-01-17 13:08:27 -0500

    functionality + multi-threading + time elapsed

--- FileTransfer/server.py
+++ FileTransfer/server.py
@@ -2,21 +2,11 @@
 import socket                   # Import socket module
 import subprocess
 import os
+import threading
 
+def Working(conn, addr):
+    start_time = time.time()
 
-count = 0
-
-port = 50000                    # Reserve a port for your service every new transfer wants a new port or you must wait.
-s = socket.socket()             # Create a socket object
-s.bind(('', port))            # Bind to the port
-s.listen(5)                     # Now wait for client connection.
-
-print ('Server listening....')
-
-
-while True:
-    conn, addr = s.accept()     # Establish connection with client.
-    print ('Got connection from', addr)
     data = conn.recv(1024)
     if data :
         print("got data")
@@ -49,6 +39,8 @@ while True:
         output, error = process1.communicate()
         output, error = process2.communicate()
 
+        elapsed_time = time.time() - start_time
+
         if error :
             msg = 'error terminating, ' + error
             conn.send(msg.encode())
@@ -59,10 +51,25 @@ while True:
         else :
             msg = 'Thank you for connecting\n\nthe result is '
             conn.send(msg.encode()+output)
-            # conn.send(output)
+            conn.send("server response time: "+elapsed_time)
             conn.close()
             bashCommand3 = "rm -fr ./" + dirname
             print (bashCommand3)
             process3 = subprocess.Popen(bashCommand3.split(), stdout=subprocess.PIPE)
         count = count+1
         # max count
+
+count = 0
+
+port = 50000                    # Reserve a port for your service every new transfer wants a new port or you must wait.
+s = socket.socket()             # Create a socket object
+s.bind(('', port))            # Bind to the port
+s.listen(5)                     # Now wait for client connection.
+
+print ('Server listening....')
+
+
+while True:
+    conn, addr = s.accept()     # Establish connection with client.
+    print ('Got connection from', addr)
+    threading.Thread(Working, (conn, addr)).start()
