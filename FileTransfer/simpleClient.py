ec634581063be0a311f77581aaa1922f99cdf493
zhan4475 <zhan4475@umn.edu>
2020-01-29 21:37:37 -0500

    + simple client for iris LR

--- FileTransfer/simpleClient.py
+++ FileTransfer/simpleClient.py
@@ -1,11 +1,18 @@
 import requests
 import time
+import threading
 
-url     = 'http://ec2-18-216-227-29.us-east-2.compute.amazonaws.com:80/predict'
+def request(url, payload, headers):
+    start   = time.time()
+    res     = requests.post(url, data=payload, headers=headers)
+    end   = time.time()
+    print(res.text)
+    print(end - start)
+
+url     = 'http://test-345378476.us-east-2.elb.amazonaws.com:80/predict'
 payload = '[6.0,  2.2, 4.0,  1.0 ]'
 headers = {'Content-Type':'application/json'}
-start   = time.time()
-res     = requests.post(url, data=payload, headers=headers)
-end   = time.time()
-print(res.text)
-print(end - start)
+for i in range(1, 10000):
+    # from time import sleep
+    time.sleep(20)
+    threading.Thread(target=request, args=(url, payload, headers)).start()
