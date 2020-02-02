0c61131f49c609e34baecb95bd552d62a0b1b765
zhan4475 <zhan4475@umn.edu>
2020-02-01 20:16:09 -0500

    to fork instead of put into my repo

--- FileTransfer/simpleClient.py
+++ FileTransfer/simpleClient.py
@@ -12,7 +12,7 @@ def request(url, payload, headers):
 url     = 'http://test-345378476.us-east-2.elb.amazonaws.com:80/predict'
 payload = '[6.0,  2.2, 4.0,  1.0 ]'
 headers = {'Content-Type':'application/json'}
-for i in range(1, 10000):
+for i in range(1, 10000000):
     # from time import sleep
-    time.sleep(20)
+    time.sleep(0.2)
     threading.Thread(target=request, args=(url, payload, headers)).start()
